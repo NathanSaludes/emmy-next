52113b186751837e7694f4fe5a8cf7424a86f238
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-01-25 00:14:43 +0800

    refactored & optimized useMenu hook

--- utils/useMenu.js
+++ utils/useMenu.js
@@ -1,36 +1,52 @@
+import { useCallback } from 'react';
 import { useEffect, useState, useContext, createContext } from 'react';
 
 const MenuContext = createContext();
-const LG_WIDTH = 1023;
+const LG_WIDTH = 1024;
 
 function useMenu() {
-	const [open, setOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false)
 	const [isMobile, setIsMobile] = useState(false)
 
-	function handleResize() {
-		if (window.innerWidth > LG_WIDTH) {
-			setOpen(() => true);
-			setIsMobile(() => false)
-		} else {
-			setOpen(() => false)
-			setIsMobile(() => true)
-		}
-	}
+	/* -----------------------------------------------------------------
+	Refactored by Alexander Marinas:
+	implemented useCallback hook for methods to improve performance since
+	we are just returning the same identity of the function.
+	----------------------------------------------------------------- */
 
-	function toggleMenu() {
-		setOpen(current => !current)
-	}
-
-	useEffect(() => {
-		window.addEventListener('resize', handleResize);
-
-		return (() => {
-			window.removeEventListener('resize', handleResize)
-		})
-
+	// this function checks if the device width is mobile
+	const handleResize = useCallback(() => {
+		console.log('Setting isMobile')
+		setIsMobile(window.innerWidth > LG_WIDTH ? false : true);
 	}, [])
 
-	return { open, toggleMenu, isMobile }
+	const toggleMenu = useCallback(() => {
+		setIsOpen(current => !current)
+	}, [])
+
+	/* -----------------------------------------------------------------
+	Side Effect: Close the menu if screen size changes.
+	----------------------------------------------------------------- */
+	useEffect(() => {
+		setIsOpen(false)
+	}, [isMobile])
+
+	/* -----------------------------------------------------------------
+	Menu Issue:
+	Menu breaks when initial page loaded on mobile screen width (< 425px)
+
+	Solution:
+	Invoke `handleResize` function first inside use effect to check
+	device screen width on mount.
+	------------------------------------------------------------------ */
+	useEffect(() => {
+		handleResize()
+		window.addEventListener('resize', handleResize);
+
+		return () => window.removeEventListener('resize', handleResize)
+	}, [handleResize])
+
+	return { isOpen, toggleMenu, isMobile }
 }
 
 export function MenuProvider({ children }) {
