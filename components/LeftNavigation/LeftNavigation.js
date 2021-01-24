b99c868de78e16568aed989457b2ebb4beb54ca7
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-01-25 01:41:58 +0800

    added initial animation state for menu/sidebar (framer motion)

--- components/LeftNavigation/LeftNavigation.js
+++ components/LeftNavigation/LeftNavigation.js
@@ -24,12 +24,13 @@ export default function LeftNavigation(props) {
 	return (
 		<motion.div
 			variants={variants}
+			initial={isMobile && { width: '0rem' }}
 			animate={isMobile ? "mobile" : "desktop"}
 			whileHover={!isMobile && { width: '16rem' }}
 			className={`fixed flex flex-col h-full py-4 bg-blue-500 text-white z-20 overflow-hidden divide-y divide-blue-400`}
 		>
 			<div className="flex items-start justify-between lg:justify-center px-4 mb-3 opacity-80 flex-shrink-0 min-w-max">
-				<img className="h-8 flex-shrink-0" src="/images/iACADEMY_Seal.svg" alt="iACADEMY" />
+				<img className="h-8 flex-shrink-0 mr-4" src="/images/iACADEMY_Seal.svg" alt="iACADEMY" />
 				<button
 					className="lg:hidden rounded-md bg-blue-400 bg-opacity-80 hover:bg-opacity-100 p-2 max-w-max focus:outline-none focus:ring-2 focus:ring-opacity-30"
 					onClick={toggleMenu}
