3fca4c8290006b468973470873164bc961881ec5
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-01-31 23:20:22 +0800

    added dashboard options (for PDF report export)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import MainLayout from '@/components/layouts/MainLayout'
 import PageHeader from '@/components/PageHeader'
+import { HiCog } from 'react-icons/hi'
+
+const DashboardOptionsButton = () => (
+	<button className={`bg-dark-denim-accent text-white py-2 px-3 rounded-lg flex focus:outline-none focus:ring-2`}>
+		<HiCog className={`text-2xl text-main`} />
+	</button>
+)
 
 export default function Dashboard() {
 	return (
@@ -9,7 +16,9 @@ export default function Dashboard() {
 				<title>Dashboard</title>
 			</Head>
 			<MainLayout>
-				<PageHeader title="Dashboard" />
+				<PageHeader title="Dashboard">
+					<DashboardOptionsButton />
+				</PageHeader>
 				<p>
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quasi harum voluptatum delectus totam possimus enim laborum laudantium saepe consequuntur quod animi, sint nisi. Eaque at dolorem illum, suscipit explicabo consequatur quod autem. Laborum voluptatem explicabo iusto porro tenetur sapiente assumenda officia quam debitis id quaerat asperiores eius maiores, ut quia totam recusandae ipsam est, atque non officiis reiciendis! Eveniet reiciendis quia iure eius ipsam. Veniam modi reprehenderit animi molestiae dicta inventore, adipisci libero recusandae saepe nobis temporibus, assumenda ratione eaque. Iusto quae fugiat doloribus, quos harum dicta sequi maxime ex voluptates. Molestiae tenetur totam facilis sed minus veniam dolorem.
 				</p>
