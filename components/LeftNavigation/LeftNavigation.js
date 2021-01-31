34bfd17fdbbc5d03fd6b91796991abf3783d3bdb
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-01-31 23:17:40 +0800

    updated color schemes (tailwind config)

--- components/LeftNavigation/LeftNavigation.js
+++ components/LeftNavigation/LeftNavigation.js
@@ -27,12 +27,12 @@ export default function LeftNavigation(props) {
 			initial={isMobile && { width: '0rem' }}
 			animate={isMobile ? "mobile" : "desktop"}
 			whileHover={!isMobile && { width: '16rem' }}
-			className={`fixed flex flex-col h-full py-4 bg-blue-500 text-white z-20 overflow-hidden divide-y divide-blue-400`}
+			className={`fixed flex flex-col h-full py-4 bg-emmy-blue text-white z-20 overflow-hidden divide-y divide-blue-400`}
 		>
 			<div className="flex items-start justify-between lg:justify-center px-4 mb-3 opacity-80 flex-shrink-0 min-w-max">
 				<img className="h-8 flex-shrink-0 mr-4" src="/images/iACADEMY_Seal.svg" alt="iACADEMY" />
 				<button
-					className="lg:hidden rounded-md bg-blue-400 bg-opacity-80 hover:bg-opacity-100 p-2 max-w-max focus:outline-none focus:ring-2 focus:ring-opacity-30"
+					className="lg:hidden rounded-md bg-emmy-blue-accent bg-opacity-80 hover:bg-opacity-100 p-2 max-w-max focus:outline-none focus:ring-2 focus:ring-opacity-30"
 					onClick={toggleMenu}
 				>
 					<HiOutlineX className="text-white text-lg" />
