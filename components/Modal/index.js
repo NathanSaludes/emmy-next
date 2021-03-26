8a880a5dfc1d7171c3fa55d466c7d5b1eb8d7a14
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-03-26 10:18:44 +0800

    Create reusable modal component. Apply overlay using portals

---
+++ components/Modal/index.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
+
+function Modal({ children }) {
+
+	const [isBrowser, setIsBrowser] = useState(false)
+	const [isOpen, setIsOpen] = useState(true)
+
+	useEffect(() => {
+		setIsBrowser(true)
+	}, [])
+
+	if (isBrowser) {
+		return ReactDom.createPortal(
+			<>
+				<div className={`fixed top-0 left-0 z-50 ${isOpen ? 'flex flex-col' : 'hidden'} justify-center items-center min-h-full min-w-full bg-black bg-opacity-70 text-primary`}>
+					{children}
+				</div>
+			</>,
+			document.getElementById("modal-root")
+		)
+	} else {
+		return null;
+	}
+}
+
+export default Modal
