34634045fb8d6b20897d203337c8ec7a5cdc8584
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-04-02 03:38:18 +0800

    Accounts Merge (04/02/2021) (#5)
    * remove custom List css
    
    * fix cursor for account action buttons
    
    * create container for List component
    
    * Create reusable modal component. Apply overlay using portals
    
    * Create initial frontend structure for Add User form (Accounts page)
    
    * create AddUserForm frontend structure
    
    * Fix AddUserForm paddings and spacing
    
    * moved sample data to folder "sample_data"
    
    * removed the margin styling of List component. Prevent unwanted margins when re-using the component in other pages.
    
    * remove scrollbar style import in _app component
    
    * custom scrollbar style for `Add User Form` component
    
    * set initial modal open state to true
    
    * replace top margin of `Input Field` component (Add User Form) to bottom margin input field spacing
    
    * Change secondary `Action Button` component with lighter text color
    
    * + add user form scrollbar styling;
    + fix form spacing;
    + add scroll for add user form component
    + add more required fields (role & password)
    
    * fix field container spacing
    
    * Fix conflicts

--- components/AddUserForm/index.js
+++ components/AddUserForm/index.js
@@ -1,30 +1,35 @@
+import styles from './scrollbar.module.scss'
+
 import InputField from './InputField'
 import ActionButton from './ActionButton'
 import { RiCloseLine } from 'react-icons/ri'
 
 function AddUserForm() {
 	return (
-		<div className={`bg-dark-denim-accent px-8 py-5 rounded-2xl`}>
-			<div className="flex flex-col items-center border-2 border-dashed border-red-500 border-opacity-0 w-80 sm:w-96">
-				<div className="flex justify-between items-baseline w-full">
-					<div className="font-semibold text-2xl tracking text-main">
-						Create New User
-					</div>
-					<button className="flex justify-center items-center h-9 w-9 bg-emmy-blue-accent bg-opacity-20 rounded-md group hover:bg-opacity-30 focus:outline-none focus:ring-2 shadow-sm">
-						<RiCloseLine className="text-2xl text-main group-hover:text-primary" />
-					</button>
+		<div className={`flex flex-col items-center bg-dark-denim-accent py-4 rounded-2xl w-96`}>
+			{/* <div className="border-2 border-dashed border-red-500 border-opacity-50"> */}
+			<div className="flex justify-between items-baseline w-full px-4 pb-3 border-dark-denim-faded">
+				<div className="ml-2 font-semibold text-2xl tracking text-main">
+					Create New User
 				</div>
-				<div className="input-group flex flex-col w-full">
-					<InputField label="First name" type="text" name="" placeholder="" />
-					<InputField label="Last name" type="text" name="" placeholder="" />
-					<InputField label="Email" type="text" name="" placeholder="" />
-					<InputField label="Username" type="text" name="" placeholder="" />
-				</div>
-				<div className="mt-8 flex justify-end w-full">
-					<ActionButton text="Cancel" isSecondary />
-					<ActionButton text="Create" type="submit" />
-				</div>
+				<button className="flex justify-center items-center h-9 w-9 bg-emmy-blue-accent bg-opacity-20 rounded-md group hover:bg-opacity-30 focus:outline-none focus:ring-2 shadow-sm">
+					<RiCloseLine className="text-2xl text-main group-hover:text-primary" />
+				</button>
 			</div>
+			<div className={`${styles.scroll} input-group flex flex-col w-full max-h-96 overflow-y-auto overflow-x-hidden pl-10 pr-4 mr-8`}>
+				<InputField label="First name" type="text" name="" placeholder="" />
+				<InputField label="Last name" type="text" name="" placeholder="" />
+				<InputField label="Email" type="text" name="" placeholder="" />
+				<InputField label="Username" type="text" name="" placeholder="" />
+				<InputField label="Account Role" type="text" name="" placeholder="" />
+				<InputField label="Password" type="password" name="" placeholder="" />
+				<InputField label="Confirm Password" type="password" name="" placeholder="" />
+			</div>
+			<div className="pt-4 px-4 flex justify-end w-full border-dark-denim-faded border-dark">
+				<ActionButton text="Cancel" isSecondary />
+				<ActionButton text="Create" type="submit" />
+			</div>
+			{/* </div> */}
 		</div>
 	)
 }
