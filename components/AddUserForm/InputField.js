6d61d7c0c8db150d2f6d1e1d9e5a289ab25332f1
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-04-01 18:46:26 +0800

    replace top margin of `Input Field` component (Add User Form) to bottom margin input field spacing

--- components/AddUserForm/InputField.js
+++ components/AddUserForm/InputField.js
@@ -4,7 +4,7 @@ function InputField({ label, type, name, placeholder }) {
 	const [errorMessage, setErrorMessage] = useState('')
 
 	return (
-		<div className="flex flex-col w-full mt-6 text-sm">
+		<div className="flex flex-col w-full mb-6 text-sm">
 			<label className="mb-1 text-main">{label}</label>
 			<div className="flex rounded-lg border-none border-emmy-blue-accent border-opacity-30 w-full overflow-hidden focus-within:ring-1">
 				<input type={type} placeholder={placeholder} name={name} className="p-4 w-full bg-dark-denim-faded text-primary text-base focus:outline-none placeholder-main placeholder-opacity-50 shadow-sm" />
